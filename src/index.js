19da1dfd12c159e5a7168b4dd2a43074d395229a
Brandon Bryan <bshwag@gmail.com>
2022-11-15 20:08:02 -0500

    added mode to fetch

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function compileWeatherDataIntoObject(data) {
 async function fetchWeatherData(location) {
   const endPoint = `https://api.weatherapi.com/v1/forecast.json?key=27c7cc7f7be44d9c8c831838221511&q=${location}&days=1&aqi=no&alerts=no`;
   try {
-    const response = await fetch(endPoint);
+    const response = await fetch(endPoint, { mode: 'cors' });
     const data = compileWeatherDataIntoObject(await response.json());
     return data;
   } catch (error) {
