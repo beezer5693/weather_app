52fa7525b3ffdd8d9b5bf0fa58cff99081a30032
Brandon Bryan <bshwag@gmail.com>
2022-11-16 20:36:52 -0500

    added comments

--- src/modules/text.js
+++ src/modules/text.js
@@ -1,9 +1,11 @@
+// Converts date to day of the week, ex: '11-16-2022' to 'Wednesday'
 function changeDateToDay(date) {
   const formattedDate = date.replace(/-/g, '/');
   const day = new Date(formattedDate).toLocaleString('en-us', { weekday: 'long' });
   return day;
 }
 
+// Removes 0 from the hour, ex: '05:00' to '5:00'
 function formatTime(str) {
   const time = str.split('');
   time.splice(0, 1);
