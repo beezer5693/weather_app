795a9d92450f650e0f82f1560f6133cff9d86cce
Brandon Bryan <bshwag@gmail.com>
2022-11-20 20:24:31 -0500

    used object destructuring for readability

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,9 +1,21 @@
-function displayCurrentWeather(data) {
-  const city = document.querySelector('.city');
-  const state = document.querySelector('.state');
+function displayCurrentWeather({
+  city,
+  state,
+  condition,
+  conditionIcon,
+  temperature,
+  feelsLike,
+  chainceOfRain,
+  wind,
+  humidity,
+  sunrise,
+  sunset,
+}) {
+  const locationCity = document.querySelector('.city');
+  const locationState = document.querySelector('.state');
   const weatherIcon = document.getElementById('weather-icon');
-  const condition = document.querySelector('.condition');
-  const temperature = document.querySelector('.temperature');
+  const locationConditions = document.querySelector('.condition');
+  const locationTemperature = document.querySelector('.temperature');
   const feelsLikeTemperature = document.querySelector('.feels-like-temperature');
   const chanceOfRainStat = document.querySelector('.chance-of-rain-stat');
   const windSpeedStat = document.querySelector('.wind-speed-stat');
@@ -11,69 +23,69 @@ function displayCurrentWeather(data) {
   const sunriseStat = document.querySelector('.sunrise-stat');
   const sunsetStat = document.querySelector('.sunset-stat');
 
-  city.innerText = `${data.city},`;
-  state.innerText = `${data.state}`;
-  condition.innerText = `${data.condition}`;
-  weatherIcon.src = `${data.conditionIcon}`;
-  temperature.innerHTML = `${data.temperature.f}&#176;`;
-  feelsLikeTemperature.innerHTML = `${data.feelsLike.f}&#176;`;
-  chanceOfRainStat.innerText = `${data.chainceOfRain}%`;
-  windSpeedStat.innerText = `${data.wind} MPH`;
-  humidityStat.innerText = `${data.humidity}%`;
-  sunriseStat.innerText = `${data.sunrise}`;
-  sunsetStat.innerText = `${data.sunset}`;
+  locationCity.innerText = `${city},`;
+  locationState.innerText = `${state}`;
+  locationConditions.innerText = `${condition}`;
+  weatherIcon.src = `${conditionIcon}`;
+  locationTemperature.innerHTML = `${temperature.f}&#176;`;
+  feelsLikeTemperature.innerHTML = `${feelsLike.f}&#176;`;
+  chanceOfRainStat.innerText = `${chainceOfRain}%`;
+  windSpeedStat.innerText = `${wind} mph`;
+  humidityStat.innerText = `${humidity}%`;
+  sunriseStat.innerText = `${sunrise}`;
+  sunsetStat.innerText = `${sunset}`;
 }
 
-function displayFiveDayForecast(data) {
-  const dayOne = document.querySelector('.day-one');
-  const dayOneIcon = document.getElementById('day-one-icon');
-  const dayOneHi = document.querySelector('.day-one-hi');
-  const dayOneLo = document.querySelector('.day-one-lo');
+function displayFiveDayForecast({ dayOne, dayTwo, dayThree, dayFour, dayFive }) {
+  const weatherDayOne = document.querySelector('.day-one');
+  const weatherDayOneIcon = document.getElementById('day-one-icon');
+  const weatherDayOneHi = document.querySelector('.day-one-hi');
+  const weatherDayOneLo = document.querySelector('.day-one-lo');
 
-  const dayTwo = document.querySelector('.day-two');
-  const dayTwoIcon = document.getElementById('day-two-icon');
-  const dayTwoHi = document.querySelector('.day-two-hi');
-  const dayTwoLo = document.querySelector('.day-two-lo');
+  const weatherDayTwo = document.querySelector('.day-two');
+  const weatherDayTwoIcon = document.getElementById('day-two-icon');
+  const weatherDayTwoHi = document.querySelector('.day-two-hi');
+  const weatherDayTwoLo = document.querySelector('.day-two-lo');
 
-  const dayThree = document.querySelector('.day-three');
-  const dayThreeIcon = document.getElementById('day-three-icon');
-  const dayThreeHi = document.querySelector('.day-three-hi');
-  const dayThreeLo = document.querySelector('.day-three-lo');
+  const weatherDayThree = document.querySelector('.day-three');
+  const weatherDayThreeIcon = document.getElementById('day-three-icon');
+  const weatherDayThreeHi = document.querySelector('.day-three-hi');
+  const weatherDayThreeLo = document.querySelector('.day-three-lo');
 
-  const dayFour = document.querySelector('.day-four');
-  const dayFourIcon = document.getElementById('day-four-icon');
-  const dayFourHi = document.querySelector('.day-four-hi');
-  const dayFourLo = document.querySelector('.day-four-lo');
+  const weatherDayFour = document.querySelector('.day-four');
+  const weatherDayFourIcon = document.getElementById('day-four-icon');
+  const weatherDayFourHi = document.querySelector('.day-four-hi');
+  const weatherDayFourLo = document.querySelector('.day-four-lo');
 
-  const dayFive = document.querySelector('.day-five');
-  const dayFiveIcon = document.getElementById('day-five-icon');
-  const dayFiveHi = document.querySelector('.day-five-hi');
-  const dayFiveLo = document.querySelector('.day-five-lo');
+  const weatherDayFive = document.querySelector('.day-five');
+  const weatherDayFiveIcon = document.getElementById('day-five-icon');
+  const weatherDayFiveHi = document.querySelector('.day-five-hi');
+  const weatherDayFiveLo = document.querySelector('.day-five-lo');
 
-  dayOne.innerText = `${data.dayOne.day}`;
-  dayOneIcon.src = `${data.dayOne.conditionIcon}`;
-  dayOneHi.innerHTML = `${data.dayOne.hi}&#176;`;
-  dayOneLo.innerHTML = `${data.dayOne.lo}&#176;`;
+  weatherDayOne.innerText = `${dayOne.day}`;
+  weatherDayOneIcon.src = `${dayOne.conditionIcon}`;
+  weatherDayOneHi.innerHTML = `${dayOne.hi}&#176;`;
+  weatherDayOneLo.innerHTML = `${dayOne.lo}&#176;`;
 
-  dayTwo.innerText = `${data.dayTwo.day}`;
-  dayTwoIcon.src = `${data.dayTwo.conditionIcon}`;
-  dayTwoHi.innerHTML = `${data.dayTwo.hi}&#176;`;
-  dayTwoLo.innerHTML = `${data.dayTwo.lo}&#176;`;
+  weatherDayTwo.innerText = `${dayTwo.day}`;
+  weatherDayTwoIcon.src = `${dayTwo.conditionIcon}`;
+  weatherDayTwoHi.innerHTML = `${dayTwo.hi}&#176;`;
+  weatherDayTwoLo.innerHTML = `${dayTwo.lo}&#176;`;
 
-  dayThree.innerText = `${data.dayThree.day}`;
-  dayThreeIcon.src = `${data.dayThree.conditionIcon}`;
-  dayThreeHi.innerHTML = `${data.dayThree.hi}&#176;`;
-  dayThreeLo.innerHTML = `${data.dayThree.lo}&#176;`;
+  weatherDayThree.innerText = `${dayThree.day}`;
+  weatherDayThreeIcon.src = `${dayThree.conditionIcon}`;
+  weatherDayThreeHi.innerHTML = `${dayThree.hi}&#176;`;
+  weatherDayThreeLo.innerHTML = `${dayThree.lo}&#176;`;
 
-  dayFour.innerText = `${data.dayFour.day}`;
-  dayFourIcon.src = `${data.dayFour.conditionIcon}`;
-  dayFourHi.innerHTML = `${data.dayFour.hi}&#176;`;
-  dayFourLo.innerHTML = `${data.dayFour.lo}&#176;`;
+  weatherDayFour.innerText = `${dayFour.day}`;
+  weatherDayFourIcon.src = `${dayFour.conditionIcon}`;
+  weatherDayFourHi.innerHTML = `${dayFour.hi}&#176;`;
+  weatherDayFourLo.innerHTML = `${dayFour.lo}&#176;`;
 
-  dayFive.innerText = `${data.dayFive.day}`;
-  dayFiveIcon.src = `${data.dayFive.conditionIcon}`;
-  dayFiveHi.innerHTML = `${data.dayFive.hi}&#176;`;
-  dayFiveLo.innerHTML = `${data.dayFive.lo}&#176;`;
+  weatherDayFive.innerText = `${dayFive.day}`;
+  weatherDayFiveIcon.src = `${dayFive.conditionIcon}`;
+  weatherDayFiveHi.innerHTML = `${dayFive.hi}&#176;`;
+  weatherDayFiveLo.innerHTML = `${dayFive.lo}&#176;`;
 }
 
 export { displayCurrentWeather, displayFiveDayForecast };
